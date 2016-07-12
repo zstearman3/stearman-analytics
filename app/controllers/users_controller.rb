47401d94561b913f9e9379365d3ab774831d4e02
Zach Stearman <zstearman3@gmail.com>
2016-07-12 19:54:01 +0000

    Add integration test for signing up users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handles a successful save.
+    else
+      render 'new'
+    end
+  end
+  
+  
+  # -------------Begin private decalrations here--------------
+  private
+  
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, 
+                                   :password, :password_confirmation)
+    end
 end
---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "User signing page" do
+  it "doesn't allow signup with invallid information" do
+    expect{signup_with("Test", "User", "test@example,com", 
+                       "foobar")}.to_not change{ User.count }
+  end
+  describe "with invalid signup information" do
+    before :each do
+      signup_with("Test", "User", "test@example,com", "foobar")
+    end
+    it "renders correct view upon signup failure" do
+      expect(page).to have_content("Sign Up!")
+    end
+    it "shows error explanation" do
+      expect(page).to have_css('div#error_explanation')
+    end
+    it "highlights fields with errors" do
+      expect(page).to have_css('div.field_with_errors')
+    end
+  end
+  
+end
+
+def signup_with(first_name, last_name, email, password)
+  visit signup_path
+  fill_in 'First Name',   with: first_name
+  fill_in 'Last Name',    with: last_name
+  fill_in 'Email',        with: email
+  fill_in 'Password',     with: password
+  fill_in 'Confirmation', with: password
+  click_button 'Create My Account'
+end
