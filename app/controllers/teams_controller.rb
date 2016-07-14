eb44d097ba8f08a72b1107e6894c0276001e393c
Zach Stearman <zstearman3@gmail.com>
2016-07-14 23:21:59 +0000

    Add show to teams resource

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,8 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.all
   end
+  
+  def show
+    @team = Team.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   post   '/login',    to: 'sessions#create'
   delete '/logout',   to: 'sessions#destroy'
   resources :users
+  resources :teams, only: [:show]
 end
